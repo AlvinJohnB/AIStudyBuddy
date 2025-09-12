ae832edd1bb63ccce5b5af37e3b7d5c6d5db7fab
alvinbregana <alvinbregana@gmail.com>
2025-09-12 23:51:59 +0800

    Added polyfill updates

--- server/ulits/polyfills.js
+++
@@ -1,6 +0,0 @@
-import { DOMMatrix, DOMPoint, DOMRect } from "canvas";
-
-global.DOMMatrix = DOMMatrix;
-global.DOMMatrixReadOnly = DOMMatrix;
-global.DOMPoint = DOMPoint;
-global.DOMRect = DOMRect;
